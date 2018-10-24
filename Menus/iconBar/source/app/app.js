a181b224fe03c2948edac9e3d3e307e48f3c166e
RrChaos <rrchaos@tutanota.com>
2018-10-24 15:33:50 +0100

    JS applied to animate menu

--- Menus/iconBar/source/app/app.js
+++ Menus/iconBar/source/app/app.js
@@ -1,1 +1,6 @@
 console.log('does it work?');
+const sec = document.querySelector("#animated");
+sec.addEventListener('click', () => {
+    let result = document.querySelector('.animatedMenu');
+    result.classList.toggle('change');
+})
