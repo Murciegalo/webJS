d087187205c4d6f59346c1595396ac6d478929fb
RrChaos <rrchaos@tutanota.com>
2018-10-24 17:10:13 +0100

    Accordion menu

--- Menus/iconBar/source/app/app.js
+++ Menus/iconBar/source/app/app.js
@@ -1,6 +1,21 @@
 console.log('does it work?');
+//ANIMATION FOR MENU BARS
 const sec = document.querySelector("#animated");
 sec.addEventListener('click', () => {
     let result = document.querySelector('.animatedMenu');
     result.classList.toggle('change');
 })
+//-----------------------------------------------------
+// ANIMATION FOR BUTTONS
+//construct array with data I need.
+const acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        const panel = this.nextElementSibling;
+        (panel.style.display === "block") ? panel.style.display = "none" :
+           panel.style.display = "block";
+  });
+}
+//-----------------------------------------------------
