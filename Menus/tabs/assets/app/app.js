9221b91c6af5e6556ff02ebbe678517ba823ca0f
RrChaos <rrchaos@tutanota.com>
2018-10-25 15:01:30 +0100

    Animated colored tabs

--- Menus/tabs/assets/app/app.js
+++ Menus/tabs/assets/app/app.js
@@ -6,7 +6,7 @@ const tabcontent = document.getElementsByClassName("tabcontent");
 //EVENTLISTENERS
 for (let i = 0;  i < handlers.length; i++){
   //make sure no Active class is populated on any other element when you run
-  handlers[i].addEventListener('click', (e) => {
+  handlers[i].addEventListener('click', () => {
     for (let i = 0; i < tabcontent.length; i++) {
         tabcontent[i].className = tabcontent[i].className.replace(" active", "");
     }
@@ -16,3 +16,16 @@ for (let i = 0;  i < handlers.length; i++){
     comparator.classList.toggle('active');
   });
 }
+// ANIMATED TABS END
+//---------------------------------------------------------------------
+const controller = document.getElementsByClassName("tablinkI");
+for (let i = 0 ; i < controller.length; i++){
+   controller[i].addEventListener('click', () =>{
+     const result = document.getElementsByClassName("tabcontentI");
+     for (let i = 0 ; i <result.length; i++){
+        result[i].className = result[i].className.replace(" activeI", "");
+     }
+      document.getElementById(controller[i].innerHTML).classList.toggle('activeI');
+   })
+}
+//------------------------------------------------------------------
