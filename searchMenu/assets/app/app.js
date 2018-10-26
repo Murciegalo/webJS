a2f020cc7fbc61cca0c9c21dedc56825ba4c262b
RrChaos <rrchaos@tutanota.com>
2018-10-26 11:11:59 +0100

    Search Menu with JS

--- searchMenu/assets/app/app.js
+++ searchMenu/assets/app/app.js
@@ -1,15 +1,15 @@
 console.log('works');
 // SEARCH menu with JS
-let search = document.getElementById('mySearch');
-//filter
-const filter = search.value.toUpperCase();
-const stock = document.getElementsByTagName('li');
-search.addEventListener('keydown', () =>{
+const search = document.getElementById('mySearch');
+search.addEventListener('keyup', () =>{
+  //filter
+  const filter = search.value.toUpperCase();
+  const stock = document.getElementsByTagName('li');
   for (let i = 0; i  < stock.length;  i++){
-    let result = stock[i].getElementsByTagName('a')[0];  //HTML (NO str)
-    if(result.innerHTML.toUpperCase().indexOf(filter) > -1)
-    {
+    let result = stock[i].getElementsByTagName('a')[0];  //H...
+    if(result.innerText.toUpperCase().indexOf(filter) > -1) {
         stock[i].style.display = ' '
-    }else { stock[i].style.display = 'none'; }
+    }else { stock[i].style.display = 'none'; } //if li not contains it, hide it
   }
 });
+//---------------------------------------
