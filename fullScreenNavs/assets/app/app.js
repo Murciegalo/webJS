a031c39dadae09a9c6473b0639fca2a426ad7e6d
RrChaos <rrchaos@tutanota.com>
2018-10-26 14:26:34 +0100

    Manipulating Navs with Offset properties in JS

--- fullScreenNavs/assets/app/app.js
+++ fullScreenNavs/assets/app/app.js
@@ -1,1 +1,21 @@
 console.log('works');
+//SHOW NAV WHEN SCROLLS + 20 px
+const rolling = () =>{
+  (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) ? document.getElementById('navbar').style.top = "0" : document.getElementById('navbar').style.top = "-70px"
+}
+
+// On scrolldown
+window.onscroll = function() {rolling()};
+//--------------------------------------------------
+// HIDE NAV WHEN USER SCROLLS DOWN AND SHOW IT WHEN SCROLLS UP
+// let prevScrollpos = window.pageYOffset;
+// window.onscroll = () => {
+//   let currentScrollPos = window.pageYOffset;
+//   (prevScrollpos > currentScrollPos) ?
+//     document.getElementById("navbar").style.top = "0" :
+//     document.getElementById("navbar").style.top = "-50px";
+//
+// prevScrollpos = currentScrollPos;
+// }
+//window.pageYOffset
+//window.pageXOffset
