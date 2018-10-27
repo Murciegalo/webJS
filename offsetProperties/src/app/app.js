c5e9b8307353b7eabff230d2c32f8e83355373ad
RrChaos <rrchaos@tutanota.com>
2018-10-27 10:52:04 +0100

    + Offset Properties in JS

--- offsetProperties/src/app/app.js
+++ offsetProperties/src/app/app.js
@@ -1,2 +1,14 @@
 console.log('works?');
 //event delegation to be added on menu
+
+window.onscroll = function(){ scrolling()}
+
+const scrolling = () =>{
+  if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+    document.getElementById('navbar').style.padding = "30px 10px"
+    document.getElementById('logo').style.fontSize = "25px";
+  }else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
