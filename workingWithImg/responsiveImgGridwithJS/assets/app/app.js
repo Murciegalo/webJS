98af91b0fe96a9c425cb933f3d106142b1c99606
RrChaos <rrchaos@tutanota.com>
2018-10-31 12:17:39 +0000

    ResponsiveImgGridWithJavaScrip

--- workingWithImg/responsiveImgGridwithJS/assets/app/app.js
+++ workingWithImg/responsiveImgGridwithJS/assets/app/app.js
@@ -1,24 +1,16 @@
 console.log('works');
 // Open
 // EVENT DELEGATION TO BE ADDED
-
-//--------------------
+const tobeDisplayed = document.getElementById('displayEdeleg');
 const stock = document.getElementsByClassName('column');
-const dis1 = () =>{
-  for (let i = 0; i < stock.length; i++){
-    stock[i].style.flex = "100%";
-  }
-}
-// 50%
-const dis2 = () =>{
-  for (let i = 0; i < stock.length; i++){
-    stock[i].style.flex = "50%";
-  }
-}
-// 25%
-const dis4 = () =>{
-  for (let i = 0; i < stock.length; i++){
-    stock[i].style.flex = "25%";
-  }
-}
+
+tobeDisplayed.addEventListener('click', (e) =>{
+    for (let i = 0;  i  < stock.length;  i++){
+        (e.target.id === "button1") ? stock[i].style.flex = "100%" :
+        (e.target.id === "button2") ? stock[i].style.flex = "50%" :
+        (e.target.id === "button4") ? stock[i].style.flex = "25%" :
+        'condition expressed wrong way';
+    }
+});
+
 //--------------------------------
