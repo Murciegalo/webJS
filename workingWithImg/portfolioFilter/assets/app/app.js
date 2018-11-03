0f694e3c6b77a82895aab364db7067e1225ffd78
RrChaos <rrchaos@tutanota.com>
2018-11-03 01:43:16 +0000

    Event Delegation JS

--- workingWithImg/portfolioFilter/assets/app/app.js
+++ workingWithImg/portfolioFilter/assets/app/app.js
@@ -1,4 +1,23 @@
 console.log('works');
 // Open
-
+const n = document.querySelectorAll('#nature');
+const c = document.querySelectorAll('#cars');
+const p = document.querySelectorAll('#people');
+document.getElementById('btnContainer').addEventListener('click', (e)=>{
+    if(e.target.name === 'nature') {
+       for (let i = 0; i < n.length; i++){
+        n[i].style.display = 'block';
+      }
+    }
+    if(e.target.name === 'cars') {
+       for (let i = 0; i < c.length; i++){
+        c[i].style.display = 'block';
+      }
+    }
+    if(e.target.name === 'cars') {
+       for (let i = 0; i < p.length; i++){
+        p[i].style.display = 'block';
+      }
+    }
+});
 //--------------------------------
