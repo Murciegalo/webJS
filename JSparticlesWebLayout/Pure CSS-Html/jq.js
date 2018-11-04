b1d9273e9b07c6c9ea945ff268db5d02c303e302
RrChaos <rrchaos@tutanota.com>
2018-11-04 21:39:49 +0000

    particlesWebLayoutDesign

---
+++ JSparticlesWebLayout/Pure CSS-Html/jq.js
@@ -0,0 +1,3 @@
+$(document).ready(fucntion(){
+          $("#particles-js").css({"height":$(window).height() + "px"});
+});      //I get JQuery to build a smart website, height & width will become self-adjusted depending on users' viewport when navigating <ou></ou>
