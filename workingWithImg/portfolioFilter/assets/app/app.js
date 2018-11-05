fe21264c54d9588b73dc147ac64584b3143f9e67
RrChaos <rrchaos@tutanota.com>
2018-11-05 11:09:45 +0000

    JS applied on PortfolioFilter

--- workingWithImg/portfolioFilter/assets/app/app.js
+++ workingWithImg/portfolioFilter/assets/app/app.js
@@ -1,23 +1,55 @@
 console.log('works');
 // Open
+// BUTTONS
 const n = document.querySelectorAll('#nature');
 const c = document.querySelectorAll('#cars');
 const p = document.querySelectorAll('#people');
 document.getElementById('btnContainer').addEventListener('click', (e)=>{
+    // CATEGORY 1
     if(e.target.name === 'nature') {
+      // SHOW ME THIS ONE
        for (let i = 0; i < n.length; i++){
         n[i].style.display = 'block';
-      }
+      } //REMOVE THIS CATEGORY
+      for (let i = 0; i < c.length; i++){
+       c[i].style.display = 'none';
+     }  //REMOVE THIS CATEGORY
+     for (let i = 0; i < p.length; i++){
+      p[i].style.display = 'none';
     }
+  }  //CATEGORY 2
     if(e.target.name === 'cars') {
        for (let i = 0; i < c.length; i++){
         c[i].style.display = 'block';
-      }
+      } //REMOVE THIS CATEGORY
+      for (let i = 0; i < n.length; i++){
+       n[i].style.display = 'none';
+     } //REMOVE THIS CATEGORY
+     for (let i = 0; i < p.length; i++){
+      p[i].style.display = 'none';
     }
-    if(e.target.name === 'cars') {
+  }  //CATEGORY 3
+    if(e.target.name === 'people') {
        for (let i = 0; i < p.length; i++){
         p[i].style.display = 'block';
       }
+      for (let i = 0; i < c.length; i++){
+       c[i].style.display = 'none';
+     }  //REMOVE THIS CATEGORY
+     for (let i = 0; i < n.length; i++){
+      n[i].style.display = 'none';
     }
+  } //ALL CATEGORIES DISPLAYED
+  if(e.target.name === 'all') {
+     for (let i = 0; i < p.length; i++){
+      p[i].style.display = 'block';
+    }
+    for (let i = 0; i < c.length; i++){
+     c[i].style.display = 'block';
+   }  //REMOVE THIS CATEGORY
+   for (let i = 0; i < n.length; i++){
+    n[i].style.display = 'block';
+  }
+}
 });
 //--------------------------------
