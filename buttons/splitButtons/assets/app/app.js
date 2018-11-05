e69452fd22bf1677f6d26329f4dc9a2f08393c4f
RrChaos <rrchaos@tutanota.com>
2018-11-05 16:17:42 +0000

    Animated buttons on JS

--- buttons/splitButtons/assets/app/app.js
+++ buttons/splitButtons/assets/app/app.js
@@ -1,3 +1,16 @@
 console.log('works');
-
+document.getElementById('myBtn').addEventListener('click', ()=>{
+    let more = document.getElementById('more');
+    let result = document.getElementById('myBtn');
+    let dots = document.getElementById('dots');
+      if (more.style.display == "none") {
+        more.style.display = 'inline';
+        result.innerHTML = "Read Less";
+        dots.style.display = 'none';
+        } else {
+          dots.style.display = "inline";
+          result.innerHTML = "Read more";
+          more.style.display = "none";
+        }
+}, false);
 //--------------------------------
