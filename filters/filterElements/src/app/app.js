30725559f33a551d2d523bbd4833bf97679060c9
RrChaos <rrchaos@tutanota.com>
2018-11-09 15:45:17 +0000

    One more filter

---
+++ filters/filterElements/src/app/app.js
@@ -0,0 +1,22 @@
+console.log('works?');
+// INPUT
+// input for all items
+const stockC = document.getElementsByClassName("filterDiv");
+console.log(stockC);
+// input for all buttons
+const stockB = document.getElementsByClassName('btn');
+console.log(stockB);
+
+// FUNCTIONS
+// +/- active class
+
+
+
+
+
+
+// HANDLERS (EVENT DELEGATION)
+const h = document.getElementById('myBtnContainer');
+h.addEventListener('click', (e) =>{
+  if(e.target.name == 'button') {}
+})
