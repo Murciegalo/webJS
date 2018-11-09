b41ffac8003a355c9133e5791e63e81dae519aa0
RrChaos <rrchaos@tutanota.com>
2018-11-09 11:06:04 +0000

    Scroll to top button with JS

--- buttons/scrolltoTopButton/assets/app/app.js
+++ buttons/scrolltoTopButton/assets/app/app.js
@@ -1,3 +1,15 @@
 console.log('works?');
+const b = document.getElementById('myBtn');
 
+window.onscroll = function(){ test();}
+const test = () => {
+  // 730 pixels away from top window
+  (document.body.scrollTop > 730 || document.documentElement.scrollTop > 730) ?
+        b.style.display = "block":
+        b.style.display = "none";
+}
+b.addEventListener('click', () =>{
+    document.body.scrollTop = 0; //Safari
+    document.documentElement.scrollTop = 0; //Chrome, Firefox, IE, Opera
+} , false)
 //------------------------------------------------------------------
