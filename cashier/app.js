6ca49c6267e98c65a578cb63ee7c07fbe415deca
RrChaos <rrchaos@tutanota.com>
2018-11-27 16:46:01 +0000

    simple Cashier on JS

--- cashier/app.js
+++ cashier/app.js
@@ -10,9 +10,11 @@ console.log('works?');
    const money = [{value: 50, name: '£50 notes: '},
     {value: 20, name: '£20 notes: '}, {value: 10, name: '£10 notes: '},  {value: 5, name: '£5 notes: '}, {value: 2, name: '£2 coins: '}, {value: 1, name: '£1 coins: '}, {value: .5, name: '£50p coins: '}, {value: .2, name: '£20p coins: '}, {value: .1, name: '£10p coins: '},
    {value: .05, name: '£5p coins'}];
-    for (let i = 0 ; i < money.length ; i++){
+   let i = 0;
+    while(change > 0){
       //Does the change amount contain this bank note?
       money[i].return = Math.floor(change / (money[i].value));
       change = (change%(money[i].value)).toFixed(2);
       money[i].return > 0 ? console.log(`${money[i].name } ${money[i].return}`) : '' ;
+      i++;
     }
