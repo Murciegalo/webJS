b74d11ca942ad1d8a5ad6e16bb2c1798e057a074
RrChaos <rrchaos@tutanota.com>
2018-11-28 23:56:36 +0000

    Closures

---
+++ closures/Iteration/app.js
@@ -0,0 +1,6 @@
+console.log('works');
+//Instructions
+// 1. Given an array
+// 2. A function will have to print to the console all items based on next  order:
+//  Index 0 is print first . lastIndex after and then one after the other from right side
+const f = run([4, 8, 1, 3]);
