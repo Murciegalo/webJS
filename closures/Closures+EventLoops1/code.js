56c800e2a3a0d52bfeb7fd5f4fec417e5008b085
RrChaos <rrchaos@tutanota.com>
2018-11-28 17:57:08 +0000

    Closures

--- closures/Closures+EventLoops1/code.js
+++ closures/Closures+EventLoops1/code.js
@@ -1,3 +1,4 @@
+// BASIC SAMPLES ON GLOBAL & LOCAL EXECUTIONS CONTEXTS AND CLOSURES
 window.console.log('does');
 //demonstration of how execution contexts work in their creation phase and its execution phase
 // Demonstration
@@ -11,4 +12,18 @@ for (var i = 0;  i < 3;  i++){
 // First execution context applied & run by JS on creation & execution phase is the Loop which it will stop when i = 3;
 // Next Execution context will run the 'onclick' function which lacks of 'i' arg so it will go up and take the value of i within for loop execution context.
 // The output on this case it will always be : 3
+
+// New EC for function(){console.log(i);} i = 3;
 //-------------------------------------------------------------------------
+// SOLUTION
+for (var i = 0;  i < 3;  i++){
+    document.getElementById(i).onclick = f(i);
+}
+
+// NEW LOCAL EXECUTION CONTEXT
+function f (j){
+  return function(){
+    console.log(j);
+  }
+}
+//Now we got a new execution context RETURN the i value each time onclick is run (as per loop for goes) getting the i value on every loop
