95895f91ea77ae49982265644cf76c0d793612fb
RrChaos <rrchaos@tutanota.com>
2018-11-28 01:09:37 +0000

    Prime numbers checker on PlainJS

--- prime/app.js
+++ prime/app.js
@@ -3,7 +3,7 @@
 //2. if the user does not provide a whole number, keep prompting the user to input a valid number until they provide one
 //3. if the provided number is prime: Print the number is prime
 //4. Otherwise print the number is not prime and print its smallest positive divisor other than 1.
-let number = parseInt(prompt('Please input a whole number'));
+// let number = parseInt(prompt('Please input a whole number'));
 
 const primeChecker = function(arg){
   if(arg <= 1){console.log(`${arg} is not a prime number`);}
@@ -20,4 +20,24 @@ const primeChecker = function(arg){
   }else {
     console.log('Sorry , input a whole number');}
 }
-primeChecker(number);
+// primeChecker(number);
+//-----------------------------------------------------------
+const myPhone = {
+  make: 'corc',
+  model: 'coroco',
+  warranty: 12,
+  color: 'orange',
+  ring: function(){
+    console.log('beep beep beep!');
+  }
+};
+const propertyGetter = function(obj){
+    let selector = Math.floor(Math.random() * 5 +1);
+    console.log(selector);
+    let stock = Object.keys(obj);
+    console.log(stock);
+    let result = stock[selector];
+
+    return obj[result];
+}
+propertyGetter(myPhone);
