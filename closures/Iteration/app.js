f82abf893f92105ce96de983c306d94e17d46b88
RrChaos <rrchaos@tutanota.com>
2018-11-29 00:07:56 +0000

    Closures

--- closures/Iteration/app.js
+++ closures/Iteration/app.js
@@ -3,4 +3,19 @@ console.log('works');
 // 1. Given an array
 // 2. A function will have to print to the console all items based on next  order:
 //  Index 0 is print first . lastIndex after and then one after the other from right side
-const f = run([4, 8, 1, 3]);
+function run(x){
+  let i = -3;
+  return function(){
+      i += 3;
+      return x[i % 4];
+    }
+}
+
+let f = run( [4, 8, 1, 3] );
+
+console.log(f());
+console.log(f());
+console.log(f());
+console.log(f());
+console.log(f());
+console.log(f());
