f367fed9dd05024be543b92303de918e51914f51
RrChaos <rrchaos@tutanota.com>
2018-12-07 10:41:48 +0000

    University Website on JS

--- uniWebsiteJS/js/app.js
+++ uniWebsiteJS/js/app.js
@@ -1,17 +1,2 @@
 // Eliminar de Local Storage
 localStorage.clear();
-let nuevoEncabezado = document.createElement('h1');
-
-nuevoEncabezado.id = 'create';
-nuevoEncabezado.textContent = 'Catálogo de Cursos';
-nuevoEncabezado.style.padding = '20px';
-
-let tobesub = document.querySelector('#encabezado');
-
-let padre = document.querySelector('#lista-cursos');
-
-
-
-padre.replaceChild(nuevoEncabezado, tobesub);
-
-console.log(nuevoEncabezado);
